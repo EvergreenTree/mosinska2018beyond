9bbb8d1398663756e9371215c67b0fbd69b4961c
Evergreenfcq <evergreencqfu@gmail.com>
2019-03-16 16:51:04 +0100

    bug fix

--- unet_server.py
+++ unet_server.py
@@ -133,9 +133,16 @@ def testGenerator(test_path,num_image = 30,target_size = (224,224),flag_multi_cl
         img = np.reshape(img,img.shape+(1,)) if (not flag_multi_class) else img
         img = np.reshape(img,(1,)+img.shape)
         yield img
+        
+def labelVisualize(num_class,color_dict,img):
+    img = img[:,:,0] if len(img.shape) == 3 else img
+    img_out = np.zeros(img.shape + (3,))
+    for i in range(num_class):
+        img_out[img == i,:] = color_dict[i]
+    return img_out / 255
 
 def saveResult(save_path,npyfile,flag_multi_class = False,num_class = 2):
     for i,item in enumerate(npyfile):
         img = labelVisualize(num_class,COLOR_DICT,item) if flag_multi_class else item[:,:,0]
-        io.imsave(os.path.join(save_path,"%d_predict.jpg"%(i+0)),img)
+        io.imsave(os.path.join(save_path,"%d_predict.png"%(i+0)),img)
 
